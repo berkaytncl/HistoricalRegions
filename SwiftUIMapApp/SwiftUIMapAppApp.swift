4a3012e7523fee71050918953a91e3724652130c
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-17 08:53:57 +0300

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Berkay Tuncel on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
