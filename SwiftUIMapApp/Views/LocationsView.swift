b3ac9bdaf5b9019f73c1b8b0e47ce6ff2da57cf6
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-18 14:58:56 +0300

    Update iPad screen and image views, landscape and launch screen

--- SwiftUIMapApp/Views/LocationsView.swift
+++ SwiftUIMapApp/Views/LocationsView.swift
@@ -11,6 +11,7 @@ import MapKit
 struct LocationsView: View {
     
     @EnvironmentObject private var viewModel: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
@@ -19,6 +20,7 @@ struct LocationsView: View {
             VStack(spacing: 0) {
                 header
                     .padding()
+                    .frame(maxWidth: maxWidthForIpad )
                 Spacer()
                 locationPreviewCard
             }
@@ -89,6 +91,8 @@ extension LocationsView {
                 LocationPreviewCardView(location: location)
                     .shadow(color: .black.opacity(0.3), radius: 20)
                     .padding()
+                    .frame(maxWidth: maxWidthForIpad)
+                    .frame(maxWidth: .infinity)
                     .transition(.asymmetric(
                         insertion: .move(edge: .trailing),
                         removal: .move(edge: .leading)))
